c2cf90bc558fa65cec931c9c532dfa2fdf78d5cd
NyamaoGregoryKaiser <nyamaokaiser79@gmail.com>
2025-05-03 10:43:26 +0300

    Add new features and improvements

--- utility-library-great-system-301/src/utils.js
+++ utility-library-great-system-301/src/utils.js
@@ -69,3 +69,38 @@ module.exports = {
   deepClone,
   debounce
 };
+/**
+ * Generates a random color in hex format
+ * @returns {string} Random hex color
+ */
+function randomColor() {
+  return '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
+}
+
+/**
+ * Formats a number with commas
+ * @param {number} num - The number to format
+ * @returns {string} Formatted number
+ */
+function formatNumber(num) {
+  return num.toString().replace(/B(?=(d{3})+(?!d))/g, ",");
+}
+
+/**
+ * Capitalizes the first letter of each word in a string
+ * @param {string} str - The string to capitalize
+ * @returns {string} Capitalized string
+ */
+function capitalizeWords(str) {
+  return str.split(' ').map(word => 
+    word.charAt(0).toUpperCase() + word.slice(1)
+  ).join(' ');
+}
+
+// Export new functions
+module.exports = {
+  ...module.exports,
+  randomColor,
+  formatNumber,
+  capitalizeWords
+};
--- utility-library-great-system-301/tests/utils.test.js
+++ utility-library-great-system-301/tests/utils.test.js
@@ -67,3 +67,19 @@ function test(name, testFn) {
     console.error(error);
   }
 }
+// Test new utility functions
+test('randomColor generates valid hex color', () => {
+  const color = randomColor();
+  expect(color).toMatch(/^#[0-9A-Fa-f]{6}$/);
+});
+
+test('formatNumber adds commas correctly', () => {
+  expect(formatNumber(1000)).toBe('1,000');
+  expect(formatNumber(1000000)).toBe('1,000,000');
+  expect(formatNumber(123)).toBe('123');
+});
+
+test('capitalizeWords capitalizes each word', () => {
+  expect(capitalizeWords('hello world')).toBe('Hello World');
+  expect(capitalizeWords('this is a test')).toBe('This Is A Test');
+});
